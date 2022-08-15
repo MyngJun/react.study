103fefac00a43d2002e40e1513af778ad228dd00
myngjun <kipjin8095@naver.com>
2022-08-15 21:52:50 +0900

    start of useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+/*앞으로 해결해야할 문제: 변하는 부분만 다시 실행되면 더 효율적이기 때문에 항상 onClick이라는 event가 실행될 때 마다 function App속 모든 코드가 실행되는 것은 불필요함*/
 import Button from "./Button"; 
 import styles from "./App.module.css";
-/*버튼의 스타일과 같은 방법으로 App.module.css에서 스타일을 결정해준 뒤 App.js에서 
-파일을 import하여 className이 title인 text에 스타일을 지정해줌*/
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);/*array에 대해 (0)을 통해 초기값을 0으로 둠 즉, 처음에 counter의 값은 0이 되며 setValue는  counter를 modify하는 function의 역할을 수행함*/
-  const onClick = () => setValue((prev) => prev + 1); /*prev의 값 즉, 이전 counter에 1이 더해져 return됨*/
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1); 
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <hi>
@@ -17,7 +21,7 @@ function App() {
       click me
       </button> 
     </div>
-  );/*첫번째 onClick은 property이자 event, 두번째 onClick은 function으로 헷갈리지 말기*/
+  );
 }
 
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,11 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.css"
-/* Button.module.css에서 만들어준 스타일을 App.js가 아닌 Button.js에 연결해줌*/
 
 function Button({text}){
 return <Button className={styles.btn}> 
         {text}
         </Button>;
-}/*function button을 Button.module.css 파일의 class 이름 btn과 연결하여 함수속 버튼에 스타일을 넣어줌*/ 
+}
 
 Button.propTypes = {
     text: PropTypes.string.isRequired,
