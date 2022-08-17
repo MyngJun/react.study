88fe81b0f893fa026a6cf38ac4dbdb30bbe77c55
myngjun <kipjin8095@naver.com>
2022-08-17 16:03:18 +0900

    resaving recap of useEffect

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ function App() {
       console.log('SEARCH FOR', keyword);
     } 
     }, [keyword]);
-  return (
+    /*keyword는 setKeywrod라는 함수에 의해 변함
+    setKeyword는 onChange라는 function에 의해 호출됨
+    onChange는 사용자 input이 변할 때 호출됨*/
+    //useEffect속 []에 array로 keyword, counter과 같이 여러 string들을 넣을 수 있음
+    //useEffect속 []이 비었을 때는 처음 실행됐을 때에만 ()속 코드가 실행됨
+    //즉, useEffect를 통해 일부 state를 변화시킬 때 component를 재실행시킨다, 즉 언제 코드를 실행시킬지 선택권을 가짐
+    return (
     <div>
       <input value={keyword} 
       onChange={onChange} 
