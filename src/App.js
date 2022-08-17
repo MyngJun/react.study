d77020242a76b13c9d2416c0b855b2d32210b8e2
myngjun <kipjin8095@naver.com>
2022-08-17 16:21:22 +0900

    set of Cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
 
+function Hello(){
+  return<h1>Hello</h1>;
+}
+
 function App() {
   const [showing, setShowing] = useState(false);
  const onClick = () => setShowing(prev => !prev);
- return <div>
+ //onclick함수가 실행될 때마다 setshowing 함수를 통해 showing의 값을 true 혹은 false로 바꿈
+ return (
+ <div>
+  {showing? <Hello/> : null}
 <button onClick={onClick}>{showing ? "Hide": "Show"}</button>
- </div>;//onClick함수를 사용하는 button을 누를 때마다 button의 text가 Hide 혹은 Show로 바뀜
-}
+ </div>
+ );
+}//{showing? <Hello/> : null} -> {a?b:c}는 a가 true라면 b를, a가 false라면 c를 실행하라는 의미
 
 export default App;
