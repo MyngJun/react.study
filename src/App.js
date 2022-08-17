b34ea3b83307b3e1f96e006a2024e1e11ef1b817
myngjun <kipjin8095@naver.com>
2022-08-17 16:30:14 +0900

    ready for Cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 
 function Hello(){
+  useEffect(()=>{
+    console.log('Im here!');
+  },[])
   return<h1>Hello</h1>;
-}
+}//  {showing? <Hello/> : null}에서 showing의 값이 true일 때마다 Hello 함수가 실행되므로 console.log('Im here!')가 showing이 true일때마다 매번 실행됨
 
 function App() {
   const [showing, setShowing] = useState(false);
@@ -15,5 +18,5 @@ function App() {
  </div>
  );
 }//{showing? <Hello/> : null} -> {a?b:c}는 a가 true라면 b를, a가 false라면 c를 실행하라는 의미
-
+//js의 button을 통해 element속 component를 create(Show)혹은 destroy(Hide)함
 export default App;
