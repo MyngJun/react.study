2e626642b22370528249510a39f45dca5964da5b
myngjun <kipjin8095@naver.com>
2022-08-17 15:32:20 +0900

    problem before second magic of useEffect

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
   const onClick = () => setValue((prev) => prev + 1); 
+  const onChange = (event) => setKeyword(event.target.value);//onChange함수가 사용될 때 argument로 event를 받음
   console.log("i run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("i run only once.");
-  };
-  useEffect(iRunOnlyOnce, []);
+  useEffect(()=> {
+    console.log("CALL THE API...");
+  }, []);/*useEffect(a,[]);에서 a에 위치하게 될 코드는 그 코드가 포함된 함수가 여러번 실행되더라도 한번만 실행됨 */
+ console.log('SEARCH FOR', keyword); //를 searchkeyword에 변화가 있을 때만 사용하고 싶지만 현재는 그렇지 않음
   return (
     <div>
+      <input value={keyword} //setKeyword함수 속 keyword라는 변수를 value로 사용함으로써 함수와 연결해줌
+      onChange={onChange} //eventlistener
+      type='text'
+      placeholder='Search here...'/>
       <hi>
         {counter}
       </hi>
