a40df7ba7dd7b535483c8c93269460ce473c69d8
myngjun <kipjin8095@naver.com>
2022-08-17 15:49:51 +0900

    result of magic in useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/*앞으로 해결해야할 문제: 변하는 부분만 다시 실행되면 더 효율적이기 때문에 항상 onClick이라는 event가 실행될 때 마다 function App속 모든 코드가 실행되는 것은 불필요함*/
 import Button from "./Button"; 
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
@@ -7,16 +6,18 @@ function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("")
   const onClick = () => setValue((prev) => prev + 1); 
-  const onChange = (event) => setKeyword(event.target.value);//onChange함수가 사용될 때 argument로 event를 받음
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   useEffect(()=> {
     console.log("CALL THE API...");
-  }, []);/*useEffect(a,[]);에서 a에 위치하게 될 코드는 그 코드가 포함된 함수가 여러번 실행되더라도 한번만 실행됨 */
- console.log('SEARCH FOR', keyword); //를 searchkeyword에 변화가 있을 때만 사용하고 싶지만 현재는 그렇지 않음
+  }, []);
+  useEffect(()=>{console.log('SEARCH FOR', keyword);
+}, [keyword]);//useEffect((a),[b])은 변수 'b'의 값이 변할 때마다 코드a가 다시 실행된다는 의미
+  /*즉,  useEffect(()=>{console.log('SEARCH FOR', keyword);}, [keyword]);는 keyword에 입력되는 값이 달라질때마다 console.log('SEARCH FOR', keyword);만 다시 실행된다.*/
   return (
     <div>
-      <input value={keyword} //setKeyword함수 속 keyword라는 변수를 value로 사용함으로써 함수와 연결해줌
-      onChange={onChange} //eventlistener
+      <input value={keyword} 
+      onChange={onChange} 
       type='text'
       placeholder='Search here...'/>
       <hi>
