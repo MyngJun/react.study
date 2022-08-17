2da7390ff6531cb1acdca3b0008d9718e24296a8
myngjun <kipjin8095@naver.com>
2022-08-17 15:50:43 +0900

    result of magid in useEffect

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ function App() {
   const [keyword, setKeyword] = useState("")
   const onClick = () => setValue((prev) => prev + 1); 
   const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
+  console.log("i run all the time"); //값이 변할 때 마다 항상 실행됨
   useEffect(()=> {
-    console.log("CALL THE API...");
+    console.log("CALL THE API...");//처음 한번만 실행됨
   }, []);
-  useEffect(()=>{console.log('SEARCH FOR', keyword);
-}, [keyword]);//useEffect((a),[b])은 변수 'b'의 값이 변할 때마다 코드a가 다시 실행된다는 의미
-  /*즉,  useEffect(()=>{console.log('SEARCH FOR', keyword);}, [keyword]);는 keyword에 입력되는 값이 달라질때마다 console.log('SEARCH FOR', keyword);만 다시 실행된다.*/
+  useEffect(()=>{
+    if(keyword !==""){
+      console.log('SEARCH FOR', keyword);
+    } 
+    }, [keyword]);//useEffect((a),[b])은 변수 'b'의 값이 변할 때마다 코드a가 다시 실행된다는 의미
+  /*즉,  useEffect(()=>{console.log('SEARCH FOR', keyword);}, [keyword]);는 keyword에 입력되는 값이 달라질때마다 console.log('SEARCH FOR', keyword);만 다시 실행된다.
+  이후 이에  if(keyword !=="")를 추가하여 ketword가 비었을 때 즉, 처음 코드가 실행되었을 때에는 실행되지 않도록 함*/
   return (
     <div>
       <input value={keyword} 
